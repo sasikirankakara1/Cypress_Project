6764d6040779b663e18450f97ab279c7ce29bac0
sasikirankakara1 <sasikirankakaraprince@gmail.com>
2026-01-18 12:36:20 +0530

    cypress commit One

--- cypress/e2e/MyProject/one.cy.js
+++ cypress/e2e/MyProject/one.cy.js
@@ -2,10 +2,11 @@
 
 describe("Launch Browser",()=>{
     beforeEach(()=>{
-        cy.visit("https://www.google.com")
+        cy.visit("https://www.cnarios.com/challenges/login-flow/")
     })
-    it.only('Google',async()=>{
+    it.only('TaskOne',async()=>{
         cy.log('Title of the Page: ',cy.url())
-        
+        cy.get("div[class='flex flex-col items-center justify-center p-2 bg-gray-50 px-4']").find("button").contains("Login").click()
+        expect(cy.get("div[class='MuiCardContent-root flex flex-col gap-4 p-6 css-15q2cw4']>div:nth-of-type(1)").should('have.text',"Both fields are required."))
     })
 })
