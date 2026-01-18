c34e82e0a789f4fc12ab653167ab04b1e45e868c
sasikirankakara1 <sasikirankakaraprince@gmail.com>
2026-01-18 12:38:51 +0530

    new commit

--- cypress/e2e/MyProject/one.cy.js
+++ cypress/e2e/MyProject/one.cy.js
@@ -4,7 +4,7 @@ describe("Launch Browser",()=>{
     beforeEach(()=>{
         cy.visit("https://www.cnarios.com/challenges/login-flow/")
     })
-    it.only('TaskOne',async()=>{
+    it.only('TaskOne - Empty fields validation',async()=>{
         cy.log('Title of the Page: ',cy.url())
         cy.get("div[class='flex flex-col items-center justify-center p-2 bg-gray-50 px-4']").find("button").contains("Login").click()
         expect(cy.get("div[class='MuiCardContent-root flex flex-col gap-4 p-6 css-15q2cw4']>div:nth-of-type(1)").should('have.text',"Both fields are required."))
